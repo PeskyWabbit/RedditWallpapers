62c431c73bded19b5e55f909c0c24bca939a6fd3
PeskyWabbit <jpeel132@gmail.com>
2018-08-29 13:17:43 -0500

    removed setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-
-setup(name='RedditWallpapers',
-      version='0.1',
-      description='Set Wallpaper',
-      executables=[Executable("RedditWallpapers.py")])
-
-
